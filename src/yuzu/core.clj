a5b0c32ba45c369466ebd59f89bd5c114a8b6736
Louie Dinh <ldinh@athinkingape.com>
2014-09-22 09:47:00 -0700

    Got stupid slurping to work

--- src/yuzu/core.clj
+++ src/yuzu/core.clj
@@ -1,7 +1,13 @@
-(ns yuzu.core (:require [clojure-mail.core :refer :all]
+(ns yuzu.core (:require [clojure.string :as string]
+                        [clojure-mail.core :refer :all]
                         [clojure-mail.message :as message]))
 
-(def messages (inbox credentials))
+(def credentials (map string/trim (string/split (string/trim-newline  (slurp "src/yuzu/credentials.secure")) #",")))
+
+
+(print credentials)
+
+(def messages (inbox (apply gen-store credentials)))
 
 (def latest (message/read-message (first messages)))
 
