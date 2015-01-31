cdc50323e977e7293e7872f407bd0778b2dcabef
Louie Dinh <ldinh@athinkingape.com>
2015-01-30 21:36:41 -0800

    Build a senders histogram

--- src/yuzu/core.clj
+++ src/yuzu/core.clj
@@ -2,16 +2,30 @@
                         [clojure-mail.core :refer :all]
                         [clojure-mail.message :as message]))
 
-(->> "src/yuzu/credentials.secure" slurp string/trim-newline (#(string/split % #",")) (map string/trim))
+(def credentials (->> "src/yuzu/credentials.secure"
+       slurp
+       string/trim-newline
+       (#(string/split % #","))
+       (map string/trim)))
 
+(def raw-messages (inbox (apply gen-store credentials)))
 
-(print credentials)
+(def messages (map message/read-message (take 10 raw-messages)))
 
-(def messages (inbox (apply gen-store credentials)))
+(defn get-sender [message]
+  (.getAddress (:from message)))
 
-(def latest (message/read-message (first messages)))
+(defn get-senders [msgs] (map get-sender msgs))
+
+(defn senders-map [msgs]
+  (reduce (fn [dict sender-addr]
+            (assoc dict
+              sender-addr
+              (inc (get dict sender-addr 0))))
+          {}
+          (get-senders msgs)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println latest))
+  (println "hello"))
